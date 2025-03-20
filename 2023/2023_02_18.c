ccfbbb8e0cd3ca990412bdb157188275d544a275
fivingo <fiving@naver.com>
2025-03-21 02:21:40 +0900

    Commit

--- 2023/2023_02_18.c
+++ 2023/2023_02_18.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
 // 2023년 2회 18번
-// 선택정렬(오름차순) 괄호 채우기
+// do-while문, sizeof() 함수, 선택정렬(오름차순)
+// 괄호 채우기
 
-int main()
-{
+int main() {
     int E[] = {64, 25, 12, 22, 11};
-    int n = sizeof(E) / sizeof(E[0]);
+    int n = sizeof(E) / sizeof(E[0]);   // sizeof(타입) | 타입 크기를 바이트 단위로 반환
     int i = 0;
 
-    do
-    {
+    do {
         int j = i + 1;
 
-        do
-        {
+        do {
+            // 문제
 //          if (E[i] ( ) E[j])
-            if (E[i] > E[j])
-            {
+            if (E[i] > E[j]) {
                 int tmp = E[i];
                 E[i] = E[j];
                 E[j] = tmp;
@@ -30,7 +28,7 @@ int main()
     } while (i < n - 1);
 
     for (int i = 0; i <= 4; i++)
-        printf("%d", E[i]);
+        printf("%d ", E[i]);
 }
 
 // [보기]
