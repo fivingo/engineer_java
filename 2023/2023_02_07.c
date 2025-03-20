fed87124ba5a88635f6ebfcf12b2fbe4341d8a95
fivingo <fiving@naver.com>
2025-03-21 02:21:40 +0900

    Commit

--- 2023/2023_02_07.c
+++ 2023/2023_02_07.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 // 2023년 2회 7번
+// for문, 4의 배수 구하기
 // 실행 결과
 
-int main()
-{
+int main() {
     int c = 0;
 
     for (int i = 1; i <= 2023; i++)
--- 2023/2023_02_09.c
+++ 2023/2023_02_09.c
@@ -1,49 +1,45 @@
 #include <stdio.h>
-#define MAX_SIZE 10
+
+#define MAX_SIZE 10     // 전역 상수 | 스택 크기
 
 // 2023년 2회 9번
+// 전역 변수, 전역 상수, 스택 알고리즘
 // 실행 결과
 
-int isWhat[MAX_SIZE];
-int point = -1;
+int isWhat[MAX_SIZE];   // 전역 변수 선언 | 스택 배열, 크기는 고정
+int point = -1;         // 전역 변수 선언 | top 위치
 
-void into(int num)
-{
+void into(int num) {    // push 연산
     if (point >= 10) printf("Full");
 
     isWhat[++point] = num;
 }
 
-int take()
-{
+int take() {            // pop 연산
     if (isEmpty() == 1) printf("Empty");
 
     return isWhat[point--];
 }
 
-int isEmpty()
-{
+int isEmpty() {         // 스택이 비었는지 확인
     if (point == -1) return 1;
 
     return 0;
 }
 
-int isFull()
-{
+int isFull() {          // 스택이 가득 찼는지 확인
     if (point == 10) return 1;
 
     return 0;
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     int e;
 
     into(5);
     into(2);
 
-    while (!isEmpty())
-    {
+    while (!isEmpty()) {
         printf("%d", take());
 
         into(4);
