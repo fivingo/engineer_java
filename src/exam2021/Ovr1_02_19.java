b5bbe328c5b80122a33fde2d4b4425ebfa657630
fivingo <fiving@naver.com>
2025-03-21 03:08:33 +0900

    Commit

---
+++ src/exam2021/Ovr1_02_19.java
@@ -0,0 +1,24 @@
+package exam2021;
+
+// 2021년 2회 19번
+// 상속, 메서드 오버라이딩
+// 실행 결과
+
+public class Ovr1_02_19 {
+    public static void main(String[] args) {
+        Ovr1_02_19 a1 = new Ovr1_02_19();
+        Ovr2 a2 = new Ovr2();
+
+        System.out.println(a1.sun(3, 2) + a2.sun(3, 2));
+    }
+
+    int sun(int x, int y) {
+        return x + y;
+    }
+}
+
+class Ovr2 extends Ovr1_02_19 {
+    int sun(int x, int y) {             // Overriding
+        return x - y + super.sun(x, y); // 상위클래스의 메서드 호출
+    }
+}
