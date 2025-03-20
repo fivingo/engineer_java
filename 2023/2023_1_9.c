5a03631f794133621de03685222227d850b1cffb
fivingo <fiving@naver.com>
2025-03-20 21:34:53 +0900

    Commit

--- 2023/2023_1_9.c
+++ 2023/2023_1_9.c
@@ -3,23 +3,21 @@
 // 2023년 1회 9번
 // 괄호 채우기
 
-int main()
-{
+int main() {
     int input = 101110;
     int di = 1;
     int sum = 0;
 
-    while (1)
-    {
+    while (1) {
         if (input == 0)
             break;
-        else
-        {
+        else {
+            // 문제
 //          sum = sum + (input ( a ) ( b )) * di;
             sum = sum + (input % 10) * di;  // 답이 4개
 //          sum = sum + (input % 5) * di;
 //          sum = sum + (input % 2) * di;
-//          sum = sum + (input & 1) * di;
+//          sum = sum + (input & 1) * di;   // 비트 연산
             di = di * 2;
             input = input / 10;
         }
